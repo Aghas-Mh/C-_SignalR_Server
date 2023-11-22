2f662f4142f9b62062bd47b88a70e516ea656d03
artash00 <artashes@inone.am>
2023-11-22 21:33:21 +0400

    Startup

---
+++ SignalRChat/ChatHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        Random rand = new Random();
+        public async Task SendMessage(string user, string message)
+        {
+            await Console.Out.WriteLineAsync("Testing");
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+        public async Task GetID(string user)
+        {
+            Console.WriteLine("GET ID CALLED");
+            int id = rand.Next();
+            await Clients.Caller.SendAsync("ReciveID", id);
+        }
+    }
+}
---
+++ SignalRChat/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+using SignalRChat;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.UseRouting();
+
+app.UseCors(CorsPolicy);
+
+app.MapHub<ChatHub>("/chatHub");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+static void CorsPolicy(CorsPolicyBuilder builder)
+{
+    builder
+     .AllowAnyOrigin()
+     .AllowAnyMethod()
+     .AllowAnyHeader();
+}
